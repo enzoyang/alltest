33726ce8e981c7d4ea74e2ab735dd7b0ae1816e7
Enzo Yang <yzb2002@gmail.com>
2010-12-27 13:50:51 +0800

    basic structure

---
+++ inlineasm/inlineasm.cpp
@@ -0,0 +1,30 @@
+// inlineasm.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+int add(int x,int y)
+{
+	_asm
+	{
+		mov eax,x //eax = x
+		add y,eax //y = y + eax
+	}
+	return y;
+}
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int x,y,sum;
+	cout << "plz input x:" << endl;
+	cin >> x;
+	cout << "plz input y:" << endl;
+	cin >> y;
+	sum = add(x,y);
+	cout << "the sum is " << sum << endl;
+	
+	system("PAUSE");
+	return 0;
+}
+
