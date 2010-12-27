138218e4431bbcd699bb4e8182fa3a75d72e0fac
Enzo Yang <yzb2002@gmail.com>
2010-12-27 14:54:16 +0800

    创建远程调用CALL工程

---
+++ remotecall/remotecallDlg.h
@@ -0,0 +1,31 @@
+// remotecallDlg.h : 头文件
+//
+
+#pragma once
+
+
+// CremotecallDlg 对话框
+class CremotecallDlg : public CDialog
+{
+// 构造
+public:
+	CremotecallDlg(CWnd* pParent = NULL);	// 标准构造函数
+
+// 对话框数据
+	enum { IDD = IDD_REMOTECALL_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
