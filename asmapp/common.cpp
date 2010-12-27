91604510a729ba4449e8557eddfac07c9f96bc47
Enzo Yang <yzb2002@gmail.com>
2010-12-27 14:30:42 +0800

    将基本加减法改为汇编加减法

--- asmapp/common.cpp
+++ asmapp/common.cpp
@@ -3,10 +3,28 @@
 
 int addMethod(int x,int y)
 {
-	return x+y;
+	//基本加法
+	//return x+y;
+
+	//汇编加法
+	_asm
+	{
+		mov eax,x
+		add y,eax
+	}
+	return y;
 }
 
 int subMethod(int x,int y)
 {
-	return x-y;
+	//基本减法
+	//return x-y;
+	
+	//汇编减法
+	_asm
+	{
+		mov eax,y
+		sub x,eax
+	}
+	return x;
 }
